df1766cdf2d7bb8170671edb73257cbd9c146615
Yaroslav Kobets <yaroslavkobetsV@gmail.com>
2023-08-13 23:35:54 +0300

    Initial Commit

---
+++ src/components/Intro.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Intro() {
+	return (
+		<div className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
+			<h1 className='text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold'>
+				Yaroslav Kobets
+			</h1>
+			<p className='text-base md:text-xl mb-3 font-medium'>Web Developer</p>
+			<p className='text-sm max-w-xl mb-6 font-bold'>
+				Hello! I am a web developer and I am looking for a company for long-term
+				collaboration. Allow me to briefly introduce myself: I am determined and
+				hardworking, with a great desire and potential for growth. I enjoy all
+				stages of web development and live for programming.
+			</p>
+		</div>
+	)
+}
+
+export default Intro
---
+++ src/data/portfolio.js
@@ -0,0 +1,26 @@
+export default [
+	{
+		title: 'etherfuse',
+		imgUrl: '/assets/etherfuse.jpg',
+		stack: ['HTML', 'SCSS', 'JavaScript', 'GSAP', 'Barba JS', 'GULP'],
+		link: 'https://etherfuse.com/',
+	},
+	{
+		title: '2 Website',
+		imgUrl: '/assets/etherfuse.jpg',
+		stack: ['HTML', 'CSS', 'JavaScript'],
+		link: 'https://github.com/',
+	},
+	{
+		title: '3 Website',
+		imgUrl: '/assets/etherfuse.jpg',
+		stack: ['HTML', 'CSS', 'JavaScript'],
+		link: 'https://github.com/',
+	},
+	{
+		title: '3 Website',
+		imgUrl: '/assets/etherfuse.jpg',
+		stack: ['HTML', 'CSS', 'JavaScript'],
+		link: 'https://github.com/',
+	},
+]
